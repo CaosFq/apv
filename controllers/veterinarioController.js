2938243c011bdc65c44dcd1332fda100c4bf6af9
CaosFq <andresromerofq@gmail.com>
2025-01-15 19:33:50 -0300

    Implementa validación de usuario, confirmación de cuenta y comprobación de contraseña en login

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -51,6 +51,30 @@ catch(error){
 
 }
    };
+const autenticar = async(req,res) => {
+    const {email, password} = req.body;
 
+    //Comprobar si el ususario existe
+    const usuario = await Veterinario.findOne({email});
 
-export{registrar, perfil, confirmar};
+    if(!usuario){
+      const error = new Error("El usuario no existe");
+      return res.status(404).json({msg: error.message});
+       }
+       //Comprobar si el usuario esta confirmado
+       if(!usuario.confirmado){
+        const error = new Error("Tu cuenta no ha sido confirmada");
+        return res.status(403).json({msg: error.message});
+       }
+       
+//Revisar el password
+if(await usuario.comprobarPassword(password)){
+   console.log("Password Correcto");
+}
+else{
+   console.log("Password incorrecto")
+}
+
+    };
+
+export{registrar, perfil, confirmar, autenticar};
--- models/Veterinario.js
+++ models/Veterinario.js
@@ -49,6 +49,11 @@ veterinarioSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+veterinarioSchema.methods.comprobarPassword = async function(
+  passwordFormulario
+) {
+  return await bcrypt.compare(passwordFormulario, this.password);
+};
 
 
 // Crear el modelo a partir del esquema
